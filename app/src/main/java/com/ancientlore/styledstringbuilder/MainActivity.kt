b8ac5e76c6bc5150bc6aa38507f232aa3f6c8cca
Nickolas Makhyboroda <ancientloregames@gmail.com>
2018-09-10 08:39:17 +0200

    added examples

--- app/src/main/java/com/ancientlore/styledstringbuilder/MainActivity.kt
+++ app/src/main/java/com/ancientlore/styledstringbuilder/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.ancientlore.styledstringbuilder
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import com.ancientlore.library.StyledStringBuilder
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+
+		val styledText = StyledStringBuilder(textField.text)
+				.forAllStartWith("O")
+				.applyTextColor(ContextCompat.getColor(applicationContext, android.R.color.holo_red_dark))
+				.forAllStartWith("R")
+				.applyBackColor(ContextCompat.getColor(applicationContext, android.R.color.holo_orange_dark))
+
+		textField.text = styledText
 	}
 }
