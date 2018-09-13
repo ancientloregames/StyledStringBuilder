db3b1752de5bcf7537f93eac22e0d3314a84c576
Nickolas Makhyboroda <ancientloregames@gmail.com>
2018-09-13 08:26:31 +0200

    added a new range pattern - for first occurrence of the word with prefix

--- library/src/main/java/com/ancientlore/library/StyledStringBuilder.kt
+++ library/src/main/java/com/ancientlore/library/StyledStringBuilder.kt
@@ -69,6 +69,18 @@ class StyledStringBuilder(val text: CharSequence): SpannableString(text) {
 	}
 
 	/**
+	 * Find and store a range in the initial string, that represents the fisrt occurrence of the word with prefix
+	 */
+	fun forFirstWith(prefix: String): StyledStringBuilder {
+		ranges.clear()
+		val pattern = Pattern.quote(prefix) + "\\w+"
+		getRanges(pattern).firstOrNull()?.let {
+			ranges.add(it)
+		}
+		return this
+	}
+
+	/**
 	 * Applies text color for all ranges, that was previously detected
 	 */
 	fun applyTextColor(@ColorInt color: Int): StyledStringBuilder {
