2e4d4d9cfe02efdba562c7ed476ef9a16fb18b22
Nickolas Makhyboroda <ancientloregames@gmail.com>
2018-09-23 09:00:41 +0200

    updated the example

--- app/src/main/java/com/ancientlore/styledstringbuilder/MainActivity.kt
+++ app/src/main/java/com/ancientlore/styledstringbuilder/MainActivity.kt
@@ -3,9 +3,9 @@ package com.ancientlore.styledstringbuilder
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
-import android.text.SpannableStringBuilder
+import android.widget.Toast
+import com.ancientlore.library.OnSpanClickListener
 import com.ancientlore.library.StyledString
-import com.ancientlore.library.toStyledString
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,23 +15,37 @@ class MainActivity : AppCompatActivity() {
 		setContentView(R.layout.activity_main)
 
 		val styledText = StyledString(textField.text)
-				.forAllStartWith("O")
-				.applyTextColor(ContextCompat.getColor(applicationContext, android.R.color.holo_red_dark))
-				.forAllStartWith("R")
+				.forFirst("text color")
+				.applyTextColor(ContextCompat.getColor(applicationContext, android.R.color.holo_green_dark))
+				.forFirst("background")
 				.applyBackColor(ContextCompat.getColor(applicationContext, android.R.color.holo_orange_dark))
-				.forAll("them")
-				.applyBackColor(ContextCompat.getColor(applicationContext, android.R.color.holo_blue_dark))
-				.forAll("all")
+				.forFirst("bold")
 				.makeBold()
-				.forAllStartWith("th")
+				.forFirst("italic")
 				.makeItalic()
-
-		val styledText2 = SpannableStringBuilder("aaa")
-				.append(" b")
-				.toStyledString()
-				.forAll()
+				.forFirst("underlined")
+				.makeUnderlined()
+				.forFirst("crossedout")
 				.crossOut()
+				.forFirst("fonts")
+				.setFont("sans-serif-thin")
+				.forFirst("rounded")
+				.applyBackColor(ContextCompat.getColor(applicationContext, android.R.color.holo_blue_light), 25)
+				.forFirst("size")
+				.setSizeDp(12)
+				.forFirst("subscripts")
+				.makeSubscript()
+				.forFirst("superscripts")
+				.makeSuperscript()
+				.forFirst("clickable")
+				.doOnClick(textField, object : OnSpanClickListener {
+					override fun onClick(span: CharSequence) {
+						Toast.makeText(this@MainActivity, R.string.clicked, Toast.LENGTH_SHORT).show()
+					}
+				})
+				.forFirst("outer links")
+				.setLink(textField, "https://github.com/ancientloregames")
 
-		textField.text = styledText2
+		textField.text = styledText
 	}
 }
